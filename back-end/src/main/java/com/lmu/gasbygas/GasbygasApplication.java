c3259d854774a53e831f7a681016c4a37fef0dfd
chama97 <chamamanchanayake5@gmail.com>
2025-01-10 14:56:27 +0530

    spring boot intialize

---
+++ back-end/src/main/java/com/lmu/gasbygas/GasbygasApplication.java
@@ -0,0 +1,13 @@
+package com.lmu.gasbygas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GasbygasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GasbygasApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/lmu/gasbygas/GasbygasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmu.gasbygas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasbygasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
